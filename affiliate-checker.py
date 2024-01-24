9de7d6f59f8a3e36cb3ccdf6c98735c1f0bd9d51
Data Dius <16731794+datadius@users.noreply.github.com>
2024-01-24 19:22:19 +0100

    first mockup for embed text

--- affiliate-checker.py
+++ affiliate-checker.py
@@ -8,34 +8,49 @@ bot = discord.Bot(intents=intent)
 
 
 class MyModal(discord.ui.Modal):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, uid_checker, *args, **kwargs) -> None:
+        self.uid_checker = uid_checker
         super().__init__(*args, **kwargs)
 
         self.add_item(discord.ui.InputText(label="UID"))
-        self.add_item(
-            discord.ui.InputText(label="Username", style=discord.InputTextStyle.long)
-        )
 
     async def callback(self, interaction: discord.Interaction):
-        embed = discord.Embed(title="Modal Results")
+        print(self.children[0].value)
+        if isinstance(self.uid_checker, Fairdesk):
+            print("Fairdesk", self.uid_checker.get_uid_info(self.children[0].value))
+        embed = discord.Embed(title="UID")
         embed.add_field(name="UID", value=self.children[0].value)
-        embed.add_field(name="Username", value=self.children[1].value)
         await interaction.response.send_message(embeds=[embed])
 
 
 class MyView(discord.ui.View):
-    @discord.ui.button(label="Fairdesk")
+    @discord.ui.button(label="Fairdesk", style=discord.ButtonStyle.primary)
     async def fairdesk_callback(self, button, interaction):
-        await interaction.response.send_modal(MyModal(title="Fairdesk"))
+        fairdesk = Fairdesk()
+        await interaction.response.send_modal(
+            MyModal(title="Fairdesk", uid_checker=fairdesk)
+        )
 
-    @discord.ui.button(label="Phemex")
+    @discord.ui.button(label="Phemex", style=discord.ButtonStyle.primary)
     async def phemex_callback(self, button, interaction):
-        await interaction.response.send_message(MyModal(title="Phemex"))
+        await interaction.response.send_modal(MyModal(title="Phemex", source="Phemex"))
+
+    @discord.ui.button(label="BingX", style=discord.ButtonStyle.primary)
+    async def bingx_callback(self, button, interaction):
+        await interaction.response.send_modal(MyModal(title="BingX", source="BingX"))
 
 
 @bot.slash_command()
 async def modal(ctx):
-    await ctx.respond(embed=discord.Embed(title="Modal via Command"), view=MyView())
+    description = ""
+    with open("text_embed.txt", "r", encoding="utf8") as f:
+        description = f.read()
+    embed = discord.Embed(
+        title="Claim Your VIP Access Today!",
+        color=discord.Color.blurple(),
+        description=description,
+    )
+    await ctx.respond(embed=embed, view=MyView())
 
 
 @bot.event
