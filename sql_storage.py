ed5b56de50df57a6cebb161b5c6bee5746dd8b5c
Data Dius <16731794+datadius@users.noreply.github.com>
2024-03-03 10:20:42 +0100

    updated users table in sqlite

--- sql_storage.py
+++ sql_storage.py
@@ -1,24 +1,30 @@
 import sqlite3
+import datetime
 
 
 class SQLAffiliate:
     def __init__(self):
-        self.db_path = "/data/affiliate.db"
+        self.db_path = "./data/affiliate.db"
 
     def initialize_db(self):
         db = sqlite3.connect(self.db_path)
         cursor = db.cursor()
         cursor.execute(
-            "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, uid TEXT)"
+            "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, uid TEXT, approvalDatetime TEXT, username TEXT, deposit INTEGER, exchange TEXT)"
         )
         cursor.execute("CREATE UNIQUE INDEX IF NOT EXISTS uid_index ON users (uid)")
+        self.updated_users_table(cursor)
         db.commit()
         db.close()
 
-    def add_user(self, uid):
+    def add_user(self, uid, username, deposit, exchange):
         db = sqlite3.connect(self.db_path)
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         cursor = db.cursor()
-        cursor.execute("INSERT INTO users (uid) VALUES (?)", (uid,))
+        cursor.execute(
+            "INSERT INTO users (uid, approvalDatetime, username, deposit, exchange) VALUES (?,?,?,?,?)",
+            (uid, now, username, deposit, exchange),
+        )
         db.commit()
         db.close()
 
@@ -31,3 +37,16 @@ class SQLAffiliate:
         if user:
             return True
         return False
+
+    def updated_users_table(self, cursor):
+        cursor.execute("PRAGMA table_info(users)")
+        columns = cursor.fetchall()
+        columns = [column[1] for column in columns]
+        if "exchange" not in columns:
+            cursor.execute("ALTER TABLE users ADD COLUMN exchange TEXT")
+        if "deposit" not in columns:
+            cursor.execute("ALTER TABLE users ADD COLUMN deposit INTEGER")
+        if "username" not in columns:
+            cursor.execute("ALTER TABLE users ADD COLUMN username TEXT")
+        if "approvalDatetime" not in columns:
+            cursor.execute("ALTER TABLE users ADD COLUMN approvalDatetime TEXT")
